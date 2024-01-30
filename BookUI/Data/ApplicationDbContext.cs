0f8229ca78073826f13a272993fef0ce9af48afa
omar <152991051+omartrjm@users.noreply.github.com>
2024-01-30 10:24:51 +0300

    add database

--- BookUI/Data/ApplicationDbContext.cs
+++ BookUI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookUI.Data
@@ -9,5 +10,14 @@ namespace BookUI.Data
             : base(options)
         {
         }
+        public  DbSet<Book> Bookks { get; set; } 
+        // add table in database named Books but in class book named Book
+        // table name will be Book cuz we defined it in class book
+        public  DbSet<Genre> Genres { get; set; } 
+        public  DbSet<ShoppingCart> ShoppingCarts { get; set; } 
+        public  DbSet<CartDetail> CartDetaila { get; set; } 
+        public  DbSet<Order> Orders { get; set; } 
+        public  DbSet<OrderDetail> OrderDetails { get; set; } 
+        public  DbSet<OrderStatus> OrderStatus { get; set; } 
     }
 }
